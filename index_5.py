9bde98ca2970d4db25dcfb1d307324c6cf3ada4d
Kayke Barbosa <104206837+Kayke-Ti@users.noreply.github.com>
2023-09-02 08:21:05 -0300

    add new commit index_5

---
+++ index_5.py
@@ -0,0 +1,10 @@
+# Solicita ao usuário que insira três números
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+numero3 = float(input("Digite o terceiro número: "))
+
+# Calcula a média dos três números
+media = (numero1 + numero2 + numero3) / 3
+
+# Exibe o resultado
+print(f"A média dos números é: {media}")
